e5e0636e374b57d8427b1c937eaae68a7fcc045c
chengshuo <chengshuo357951@gmail.com>
2014-06-27 22:36:08 +0800

    before a b optimize

--- othello.cpp
+++ othello.cpp
@@ -6,12 +6,15 @@
 
 class othello_ai{
 	othello16 o;//实例化othello16类
+	int glb_depth;
 	public:
 	void init(int color, string s);
 	void move(int color, int x, int y);
 	pair<int, int> get();
 	int get_eval(string s,int color);   //算出给定局面的估价值
-	int value(int x,int y,int col);   //获取每一个点的估价值
+	int value(int x,int y);   //获取每一个点的估价值
+	int begin_value(int x,int y);
+	double mid_score();
 	int eval_node(int depth,int color,int cmp_val);  //返回倒退值
 };
 
@@ -30,6 +33,7 @@ pair<int, int> othello_ai::get(){
 	string ori_string;
 	vector<pair<int,int> > ans;
 
+	glb_depth = 4;
 	ori_string = o.tostring(); //保存初始局面
 	b_index = 0;
 	b_value = -0x4000;
@@ -39,33 +43,57 @@ pair<int, int> othello_ai::get(){
 
 	init_color = (o.mycolor==1)?2:1;
 
-	std::cerr<<"root-->allmove size is "<<ans_size<<endl;		
+	//position list contain corner,return directly
+	for(int i=0; i<ans_size; i++)
+		if((ans[i].first == 0 && ans[i].second == 0)
+			|| (ans[i].first == 0 && ans[i].second == 15)
+			|| (ans[i].first == 15 && ans[i].second == 0)
+			|| (ans[i].first == 15 && ans[i].second == 15)
+			)
+		{
+			std::cerr<<"get corner:<,"<<ans[i].first<<","<<ans[i].second<<">\n";
+			return ans[i];
+		}			
+
+
+	std::cerr<<"\nroot--->move size: "<<ans_size<<endl;		
 	for(int i=0; i<ans_size; i++){
 		o.play(o.mycolor,ans[i].first,ans[i].second);
 		temp_value = eval_node(depth+1,init_color,b_value);
-		std::cerr<<"root--->move["<<i<<"] value is "<<temp_value<<endl;
+		std::cerr<<"move["<<i<<"] value:"<<temp_value<<endl;
 		o.init(o.mycolor,ori_string);
+
+		if(temp_value == 0x40000 || temp_value == -0x40000)
+			break;
+
+		if(temp_value == 0x50000){ //op can't move
+			std::cerr<<"move["<<i<<"],op can't move in serch end...\n";
+			return ans[i];
+		}
+
 		if(b_value < temp_value){
 			b_value = temp_value;
 			b_index = i;
 		}
 	}	
-	std::cerr<<"root--->choose move["<<b_index<<"]\n";
+	std::cerr<<"root--->choose move["<<b_index<<"]\n\n";
 	return ans[b_index];
 }
 
 
 
 int othello_ai::eval_node(int depth,int color,int cmp_val){
-	if(get_time()>1995){ //超时
+	if(get_time()>1990){ //超时
 		std::cerr<<"timeout..\n";
+		return (depth%2==0)?0x40000:-0x40000;
+	}
+	if(depth == glb_depth){  // 达到指定的搜索深度
 		return get_eval(o.tostring(),color);
 	}
-	if(depth == 4)  // 达到指定的搜索深度
-		return get_eval(o.tostring(),color);
 
 	int best_value,temp_value;
 	int op_col;
+	int end_op;
 	vector<pair<int,int> >  ans; 
 	int chd_size;
 	string ori_string; //保持起初的局面
@@ -74,8 +102,22 @@ int othello_ai::eval_node(int depth,int color,int cmp_val){
 	best_value = (depth%2==0)?-0x40000:0x40000;
 	ori_string = o.tostring();
 
-	if(o.canmove(color) == false){  //color颜色方无法走
-		return (depth%2==0)?-0x40000:0x40000;
+	if(!o.canmove(color)){  //color颜色方无法走
+		if(!o.canmove(op_col)){  //end								
+			if(o.count(o.mycolor) >= 128){
+				std::cerr<<"get end,max win\n";
+				return 0x50000;     //root choose num				
+			}
+			else{
+				std::cerr<<"get end,min win\n";
+				if(depth == 1)
+					return -0x80000;
+				else
+					return (depth%2==0)?-0x80000:0x80000;
+			}								
+		} else {  //not end
+			return get_eval(o.tostring(),color);
+		}
 	}
 
 	ans = o.allmove(color);  //获取所有可下位置列表
@@ -84,6 +126,15 @@ int othello_ai::eval_node(int depth,int color,int cmp_val){
 	for(int i=0;i<chd_size;i++){
 		o.play(color,ans[i].first,ans[i].second);
 		temp_value = eval_node(depth+1,op_col,best_value);
+		o.init(o.mycolor,ori_string);
+
+		if(temp_value == 0x40000 || temp_value == -0x40000){ //child node timeout
+			std::cerr<<"test2\n";
+			return 0x40000;
+		}
+
+		if(temp_value == 0x50000)  //op can't move
+			return 0x50000;	
 		
 		if(depth % 2 == 0 && cmp_val <= temp_value){   //减枝			
 			return temp_value;
@@ -92,64 +143,106 @@ int othello_ai::eval_node(int depth,int color,int cmp_val){
 			return temp_value;
 		}
 
-		//std::cerr<<depth<<"--ans["<<i<<"] value is "<<temp_value<<endl;
-		o.init(o.mycolor,ori_string);
 		if(depth % 2 ==0 && best_value < temp_value)
 			best_value = temp_value;
 		else if(depth % 2 != 0 && best_value > temp_value)
 			best_value = temp_value;
 	}
-	//std::cerr<<depth<<"--node value is "<<best_value<<endl;
 	return best_value;
 }
 
 
 
 //行动力与棋子分值各占50%
-int othello_ai::get_eval(string s,int color){
-	int sum;
+int othello_ai::get_eval(string s,int color)
+{
+	int be_value = 0; 		
+	double score;
+	int p_val;	
 	int x,y;
-	int op_col;
-	int eval;
+	int op_color = (o.mycolor==1)?2:1;
 
-	eval = 0;
-	sum = 0;
-	op_col = (o.mycolor==1)?2:1;
-
-	for(int i=0; i<s.size();i++){
-		x = i/16;
-		y = i%16;
-		if(s.at(i)-0x30 == o.mycolor)
-			sum += value(x,y,o.mycolor);
-		else if(s.at(i)-0x30 == op_col)
-			sum -= value(x,y,op_col);
-	}
-
-	// eval += 0.5*sum;
-
-	/*op_col = (color==1)?2:1;   //next person to put
-	if(o.canmove(color)){
-		if(color== o.mycolor)  //下一步我方走
-			eval += 0.5*o.allmove(color).size();
+	//计算开局时的估价函数
+	if(o.count(0) > 212){		
+		// p_val = 0;
+		// for(int i=0; i<16; i++)
+		// 	for(int j=0; j<16; j++)
+		// 		if(o.is(o.mycolor,i,j))
+		// 			p_val--;
+		// 		else if(o.is(op_color,i,j))
+		// 			p_val++;
+		// score = p_val;
+		int size1=0, size2=0;
+		int mv_val = 0;
+		if(o.canmove(o.mycolor))
+			size1 = o.allmove(o.mycolor).size();
+		if(o.canmove(op_color))
+			size2 = o.allmove(op_color).size();
+		if(size1 > size2)
+			mv_val = 100.0 * size1 / (size1 + size2);
+		else if(size1 < size2)
+			mv_val = -100.0 * size2 / (size1 + size2);
 		else 
-			eval -= 0.5*o.allmove(color).size();
+			mv_val = 0;
+		score = mv_val;
 	}
-	else if(o.canmove(op_col)){
-		if(op_col== o.mycolor)
-			eval += 0.5*o.allmove(op_col).size();
-		else 
-			eval -= 0.5*o.allmove(op_col).size();
+	//中间阶段的估价函数
+	else if(o.count(0) > 16){
+		score = mid_score();
 	}
+	//最后阶段
 	else{
-		//std::cerr<<"game end\n";
-	}*/
+		p_val = 0;
+		for(int i=0; i<16; i++)
+			for(int j=0; j<16; j++)
+				if(o.is(o.mycolor,i,j))
+					p_val++;
+				else if(o.is(op_color,i,j))
+					p_val--;
+		score = p_val;
+	}		
 
-	return sum;
+	return score;
 }
 
+int othello_ai::value(int x,int y)
+{
+	int i,j;	
+	int val[16][16] = {
+		{40,0,10,10,10,8,8,8},
+		{0,0,-2,-2,-2,-2,-2,-2},
+		{10,-2,6,6,6,6,6,6},
+		{10,-2,6,3,3,3,3,3},
+		{10,-2,6,3,4,4,4,4},
+		{8,-2,6,3,4,2,2,2},
+		{8,-2,6,3,4,2,-2,-2},
+		{8,-2,6,3,4,2,-2,-4}
+		// {40,-12,28,24,20,16,12,8},
+		// {-12,-16,-12,-8,-6,-2,1,6},
+		// {28,-12,20,16,12,10,8,4},
+		// {24,-8,16,12,10,8,6,3},
+		// {20,-6,12,10,8,6,4,2},
+		// {16,-2,10,8,6,4,2,1},
+		// {12,1,8,6,4,2,1,0},
+		// {8,6,4,3,2,1,0,-1}
+	};	
 
+	for(i=0;i<8;i++)
+		for(j=8; j<16; j++)
+			val[i][j] = val[i][15-j];
 
-int othello_ai::value(int x,int y,int col){
+	for(i=8; i<16; i++)
+		for(j=0; j<8; j++)
+			val[i][j] = val[15-i][j];
+
+	for(i=8; i<16; i++)
+		for(j=8; j<16; j++)
+			val[i][j] = val[15-i][15-j];
+	
+		return val[x][y];	
+}
+
+int othello_ai::begin_value(int x,int y){
 	if((x==0 && y==0) || (x==0 && y==15) || (x==15 && y==0) || (x==15 && y==15)) //corner
 		return 100;
 
@@ -171,5 +264,126 @@ int othello_ai::value(int x,int y,int col){
 	return 1;
 }
 
+double othello_ai::mid_score()
+{
+	int my_pown=0,op_pown=0;	
+	int p_val = 0;
+	int my_corner = 0, op_corner = 0;
+	int my_c_s = 0, op_c_s = 0;
+	int my_front_pown=0, op_front_pown=0;
+	int round_x[] = {-1, -1, 0, 1, 1, 1, 0, -1};
+	int round_y[] = {0, 1, 1, 1, 0, -1, -1, -1};
+	int op_color = (o.mycolor==1)?2:1;
+	int x,y;	
+	double score;
+	double pown_val, mv_val, front_val, corner_val, side_val;
+
+	int count = 0;
+	for(int i=0; i<16; i++)
+		for(int j=0; j<16; j++){
+			if(o.is(o.mycolor,i,j)){
+				p_val += value(i,j);
+				my_pown++;
+			}
+			else if(o.is(op_color,i,j)){
+				p_val -= value(i,j);
+				op_pown++;
+			}
+			count = 0;
+			if(!o.is(0,i,j)){			
+				for(int k = 0; k<8; k++){
+					x = i + round_x[k];
+					y = j + round_y[k];
+					if(x>=0 && x<16 && y>=0 && y<16 && o.is(0,x,y)){						
+						if(o.is(o.mycolor,i,j))				
+							my_front_pown++;
+						else 
+							op_front_pown++;
+						break;
+					}
+				}				
+			}
+		}
+	//num of pown
+	if(my_pown > op_pown)
+		pown_val = 100.0*my_pown /(my_pown + op_pown);
+	else if(my_pown < op_pown)
+		pown_val = -100.0*op_pown/ (my_pown + op_pown);
+	else	
+		pown_val = 0;
+
+	// pown_val = 100.0*(my_pown-op_pown)/ (my_pown + op_pown);
+
+	// num of pown outside
+	if(my_front_pown > op_front_pown)
+		front_val = -100.0*my_front_pown/(my_front_pown + op_front_pown);
+	else if(my_front_pown < op_front_pown)
+		front_val = 100.0 * op_front_pown/ (my_front_pown + op_front_pown);
+	else 
+		front_val = 0;
+	// front_val = 100.0 * (my_front_pown - op_front_pown)/(my_front_pown + op_front_pown);
+
+	//corner 
+	if(o.is(o.mycolor,0,0)) my_corner++;
+	else if(o.is(op_color,0,0)) op_corner++;
+	if(o.is(o.mycolor,0,15)) my_corner++;
+	else if(o.is(op_color,0,15)) op_corner++;
+	if(o.is(o.mycolor,15,0)) my_corner++;
+	else if(o.is(op_color,15,0)) op_corner++;
+	if(o.is(o.mycolor,15,15)) my_corner++;
+	else if(o.is(op_color,15,15)) op_corner++;
+	corner_val = 25 * (my_corner - op_corner);
+
+	//corner side
+	if(o.is(0,0,0)){
+		if(o.is(o.mycolor,0,1)) my_c_s++;
+		else if(o.is(op_color,0,1)) op_c_s++;
+		if(o.is(o.mycolor,1,1)) my_c_s++;
+		else if(o.is(op_color,1,1)) op_c_s++;
+		if(o.is(o.mycolor,1,0)) my_c_s++;
+		else if(o.is(op_color,1,0)) op_c_s++;
+	}
+	if(o.is(0,0,15)){
+		if(o.is(o.mycolor,0,14)) my_c_s++;
+		else if(o.is(op_color,0,14)) op_c_s++;
+		if(o.is(o.mycolor,1,14)) my_c_s++;
+		else if(o.is(op_color,1,14)) op_c_s++;
+		if(o.is(o.mycolor,1,15)) my_c_s++;
+		else if(o.is(op_color,1,15)) op_c_s++;
+	}
+	if(o.is(0,15,0)){
+		if(o.is(o.mycolor,15,1)) my_c_s++;
+		else if(o.is(op_color,15,1)) op_c_s++;
+		if(o.is(o.mycolor,14,1)) my_c_s++;
+		else if(o.is(op_color,14,1)) op_c_s++;
+		if(o.is(o.mycolor,14,0)) my_c_s++;
+		else if(o.is(op_color,14,0)) op_c_s++;
+	}
+	if(o.is(0,15,15)){
+		if(o.is(o.mycolor,15,14)) my_c_s++;
+		else if(o.is(op_color,15,14)) op_c_s++;
+		if(o.is(o.mycolor,14,14)) my_c_s++;
+		else if(o.is(op_color,14,14)) op_c_s++;
+		if(o.is(o.mycolor,14,15)) my_c_s++;
+		else if(o.is(op_color,14,15)) op_c_s++;
+	}
+	side_val = -12.5 * (my_c_s - op_c_s);
+
+	//mobility
+	int size1=0, size2=0;
+	if(o.canmove(o.mycolor))
+		size1 = o.allmove(o.mycolor).size();
+	if(o.canmove(op_color))
+		size2 = o.allmove(op_color).size();
+	if(size1 > size2)
+		mv_val = 100.0 * size1 / (size1 + size2);
+	else if(size1 < size2)
+		mv_val = -100.0 * size2 / (size1 + size2);
+	else 
+		mv_val = 0;
+	// mv_val = 100.0 * (size1 - size2) / (size1 + size2);
+	score = 10*pown_val + 800 * corner_val + 400 * side_val + 79 * mv_val + 80*front_val + 11 * p_val;
+	return score;
+}
 
 
